1b9af6d521022a145c725dcc6c94e5e33b3a433d
stephancarpin <viviancarpin@gmail.com>
2022-02-01 11:53:24 +0400

    add stop with no hand + 3 finger chord

---
+++ custom.js
@@ -0,0 +1,200 @@
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+//const canvasElementOver = document.getElementsByClassName('output_canvas_over')[0];
+const canvasCtx = canvasElement.getContext('2d');
+let x, x_thumb, x_POINTER, x_middle, x_ring, x_pinky = null;
+let y, y_thumb, y_POINTER, y_middle, y_ring, y_pinky = null;
+//buffer
+let margin = 0.05;
+
+let last_x_thumb = 0;
+let thumb_play = 0;
+let previous_thumb_play,previous_POINTER_play,previous_middle_play = 0;
+
+let last_x_POINTER = 0;
+let POINTER_play = 0;
+
+let last_x_middle = 0;
+let middle_play =0;
+console.log(last_x_thumb);
+
+
+let midiOutput = null;
+const NOTE_ON = 0x90;
+const NOTE_OFF = 0x80;
+const CONTROL_CHANGE = 0xB0;
+
+const NOTE_DURATION = 300;
+
+
+const playNote = function (note) {
+
+
+    note = Math.ceil(range(note));
+    midiOutput.send([NOTE_ON, note, 0x7f]);
+    //midiOutput.send([NOTE_OFF, notes, 0x7f], window.performance.now() + 1000.0);
+    //midiOutput.send([CONTROL_CHANGE,thumbsPositionFilter,0x7f]);
+}
+const playNoteOff = function (note) {
+   //midiOutput.send([NOTE_ON, notes, 0x7f]);
+    note = Math.ceil(range(note));
+    midiOutput.send([NOTE_OFF, note, 0x7f], window.performance.now() + 1000.0);
+
+}
+
+navigator.requestMIDIAccess()
+    .then(function (midiAccess) {
+        const outputs = midiAccess.outputs.values();
+        console.log(outputs);
+        for (const output of outputs) {
+            console.log(output);
+            midiOutput = output;
+        }
+
+    });
+
+
+function range(input) {
+
+    if ((input * 105.83) > 127) {
+        return 127;
+
+    } else {
+        return input * 105.83
+    }
+
+}
+
+function drawPointer(x, y) {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.lineWidth = "6";
+    ctx.strokeStyle = "red";
+    ctx.arc(x, y, 50, 0, 2 * Math.PI);
+    //ctx.rect(x, y, 400, 400);
+    ctx.stroke();
+}
+
+function onResults(results) {
+
+
+
+   if(results.multiHandLandmarks.length === 0)
+   {
+      // console.log('no hands');
+       playNoteOff(previous_thumb_play);
+       playNoteOff(previous_middle_play);
+       playNoteOff(previous_POINTER_play);
+   }
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    if (results.multiHandLandmarks) {
+        for (const landmarks of results.multiHandLandmarks) {
+
+            x_thumb = results.multiHandLandmarks[0][4].x;
+            y_thumb = results.multiHandLandmarks[0][4].y;
+
+            if ((x_thumb > (last_x_thumb + margin)) || (x_thumb < (last_x_thumb - margin)) || last_x_thumb === 0) {
+                last_x_thumb = x_thumb;
+
+                thumb_play = 1
+            }
+
+            x_POINTER = results.multiHandLandmarks[0][8].x;
+            y_POINTER = results.multiHandLandmarks[0][8].y;
+
+
+            if ((x_POINTER > last_x_POINTER + margin) || (x_POINTER < last_x_POINTER - margin) || last_x_POINTER === 0) {
+                last_x_POINTER = x_POINTER;
+
+                POINTER_play = 1;
+
+            }
+
+            x_middle = results.multiHandLandmarks[0][12].x;
+            y_middle = results.multiHandLandmarks[0][12].y;
+
+
+            if ((x_middle > last_x_middle + margin) || (x_middle < last_x_middle - margin) || last_x_middle === 0) {
+                last_x_middle = x_middle;
+
+                middle_play = 1;
+
+            }
+
+            /**Play Chenged NOtes **/
+
+
+            if (thumb_play === 1) {
+                thumb_play = 0;
+
+                playNoteOff(previous_thumb_play);
+
+                previous_thumb_play = last_x_thumb;
+
+
+                playNote(last_x_thumb);
+            }
+            if (POINTER_play === 1) {
+
+                POINTER_play = 0;
+                playNoteOff(previous_POINTER_play);
+                previous_POINTER_play = last_x_POINTER;
+                playNote(last_x_POINTER);
+            }
+            if (middle_play === 1) {
+                previous_middle_play = last_x_middle;
+                playNoteOff(previous_middle_play);
+                middle_play = 0;
+                playNote(last_x_middle);
+            }
+
+            drawPointer(last_x_thumb * 1280, y_thumb * 720);
+            drawPointer(last_x_POINTER * 1280, y_POINTER * 720);
+            drawPointer(last_x_middle * 1280, y_middle * 720);
+
+            x_middle = results.multiHandLandmarks[0][12].x;
+            y_middle = results.multiHandLandmarks[0][12].y;
+
+
+            x_ring = results.multiHandLandmarks[0][16].x;
+            y_ring = results.multiHandLandmarks[0][16].y;
+
+            // setTimeout(function () {
+            //
+            //
+            // }, 300);
+            //drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 5 });
+            //drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+        }
+    }
+    canvasCtx.restore();
+}
+
+const hands = new Hands({
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    }
+});
+hands.setOptions({
+    maxNumHands: 2,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+});
+
+hands.onResults(onResults);
+
+
+const camera = new Camera(videoElement, {
+    onFrame: async () => {
+        await hands.send({image: videoElement});
+    },
+    width: 1280,
+    height: 720
+});
+camera.start();
